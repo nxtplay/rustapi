fde9c1005e4c6d481010502aef8f81e5eda42f54
Will Metz <44082458+William-Metz@users.noreply.github.com>
2024-03-25 10:08:26 -0700

    Made it runnable on ec2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_cors::Cors;
 use actix_web::{web, App, HttpServer};
+use actix_web::http::header;
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
 use dotenv::dotenv;
@@ -11,7 +12,6 @@ mod handlers;
 use crate::handlers::video::{get_upload_url, fetch_videos};
 use crate::handlers::auth::login_page;
 
-use log::{debug, error, log_enabled, info, Level};
 // The main function is the entry point for the program
 // It is used to start the server and listen for incoming requests
 // The server listens for incoming requests and routes them to the appropriate handler
@@ -25,32 +25,48 @@ async fn main() -> anyhow::Result<()> {
     println!("Starting the Rust API server...");
     dotenv().ok();
 
-    //let database_url = "postgres://willmetz:Raventhree2020@host.docker.internal:5432/nxtplaydatabase";
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-//    let database_url = "postgres://postgres:uXmbs3dNgEH0ACKntrMQ@nxtplaydatabase.cxee8am8a74x.us-west-1.rds.amazonaws.com:5432/nxtplaydatabase";
+
     // Connect to the database
-    let manager = PostgresConnectionManager::new_from_stringlike(database_url, NoTls)?;
     println!("Connecting to the database...");
-   // let pool = Pool::builder().max_size(16).build(manager).await?;
-    let pool = Pool::builder()
-    .max_size(16) // Example size
-    .build(manager).await
-    .map_err(|e| {
-        println!("Failed to create connection pool: {}", e);
-        e // You can also transform the error here
-    })?;
+    println!("Database URL: {}", database_url);
+   let manager = PostgresConnectionManager::new_from_stringlike(database_url, NoTls)?;
+   let pool = bb8::Pool::builder().build(manager).await.expect("Failed to create pool.");   
+//   let pool = Arc::new(pool); // Wrap the pool in an Arc for shared ownership
+
+ //   let pool_clone = pool.clone();
+    
+//j    println!("Trying to fetch videos...");
+ //   let conn = pool.get().await?;
+   // let rows = conn.query("SELECT description, videouid FROM Videos", &[])
+    //    .await
+      //  .expect("Failed to execute query");
+
+ //   let videos: Vec<Video> = rows.iter().map(|row| Video {
+   //     title: row.get("description"),
+     //   video_uid: row.get("videouid"),
+   // }).collect();
+   // println!("Videos: {:?}", videos);
+
+
     //Initialize Firebase Auth
     let firebase_auth = FirebaseAuth::new("nxtplay-9dbae").await;
     println!("Firebase Auth initialized...");
     println!("Server running at http:// ");
     HttpServer::new(move || {
-        let cors = Cors::permissive()// Adjust according to your needs
-            .allowed_origin("http://localhost:3000") // React app's origin
-            .allowed_origin("http://134.173.248.4") // React app's origin
+        let cors = Cors::default()
+        .allow_any_origin()
+        .allowed_methods(vec!["GET", "POST", "OPTIONS"])
+        .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT, header::CONTENT_TYPE])
+        .max_age(3600); 
+       // let cors = Cors::permissive()// Adjust according to your needs
+        //    .allowed_origin("http://localhost:3000") // React app's origin
+         //   .allowed_origin("http://134.173.248.4") // React app's origin
+          //  .allowed_origin("http://134.173.92.5")
 //            .allowed_methods(vec!["GET", "POST",  "OPTIONS"])
  //           .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT, http::header::CONTENT_TYPE])
   //          .allowed_header(http::header::CONTENT_TYPE)
-            .max_age(3600);
+           // .max_age(3600);
         App::new()
             .wrap(cors)
             .app_data(web::Data::new(pool.clone()))
